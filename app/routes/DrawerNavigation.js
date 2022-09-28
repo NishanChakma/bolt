7da08d59d4e88a92a45dec62298c250685618923
Nishan IT <nishancse13@gmail.com>
2022-09-28 16:32:44 +0600

    Update DrawerNavigation.js

--- app/routes/DrawerNavigation.js
+++ app/routes/DrawerNavigation.js
@@ -1,18 +1,45 @@
 import * as React from 'react';
 import Home from '../screens/Home';
 import {createDrawerNavigator} from '@react-navigation/drawer';
+import {bindActionCreators} from 'redux';
+import {ActionCreators} from '../store';
+import {useDispatch, useSelector} from 'react-redux';
+import {useNavigation} from '@react-navigation/native';
+import {useEffect} from 'react';
 
 const Drawer = createDrawerNavigator();
 
+const LogOut = () => {
+  const navigation = useNavigation();
+  const dispatch = useDispatch();
+  const verify = useSelector(state => state.AuthReducer.verify); //store
+  const {LogOut} = bindActionCreators(ActionCreators, dispatch);
+  useEffect(() => {
+    if (verify) {
+      LogOut(navigation);
+    } else {
+      navigation.navigate('Login');
+    }
+  }, [verify]);
+  return null;
+};
+
 function DrawerNav() {
+  const checkVerify = useSelector(state => state.AuthReducer.verify); //store
   return (
     <Drawer.Navigator initialRouteName="Home">
       <Drawer.Screen
         name="Home"
         component={Home}
-        // options={{drawerLabel: 'Home'}}
         options={{headerShown: false}}
       />
+      {checkVerify && (
+        <Drawer.Screen
+          name="Logout"
+          component={LogOut}
+          options={{headerShown: false}}
+        />
+      )}
     </Drawer.Navigator>
   );
 }
